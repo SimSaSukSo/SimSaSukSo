80ccb21472c284c17759138c29ec946411e662d3
so0nim <308go@naver.com>
2021-06-24 22:27:41 +0900

    modify Home 화면 레이아웃

---
+++ SimSaSukSo/Sources/Home/BestTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BestTabViewController.swift
+//  SimSaSukSo
+//
+//  Created by 소영 on 2021/06/24.
+//
+
+import UIKit
+
+class BestTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SimSaSukSo/Sources/Home/HomeViewController.swift
+++ SimSaSukSo/Sources/Home/HomeViewController.swift
@@ -9,8 +9,70 @@ import UIKit
 
 class HomeViewController : UIViewController {
     
+    var pageViewController : PageViewController!
+    
+    @IBOutlet var homeTabButton: UIButton!
+    @IBOutlet var bestTabButton: UIButton!
+    @IBOutlet var newTabButton: UIButton!
+    
+    var buttonLists: [UIButton] = []
+    var currentIndex : Int = 0 {
+        didSet {
+            changeButtonColor()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    //MARK: - Function
+    
+    func setButtonList() {
+        buttonLists.append(homeTabButton)
+        buttonLists.append(bestTabButton)
+        buttonLists.append(newTabButton)
+        
+        homeTabButton.tintColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+        bestTabButton.tintColor = #colorLiteral(red: 0.6509803922, green: 0.6901960784, blue: 0.7294117647, alpha: 1)
+        newTabButton.tintColor = #colorLiteral(red: 0.6509803922, green: 0.6901960784, blue: 0.7294117647, alpha: 1)
+        
+
+    }
+    
+    func changeButtonColor() {
+        for (index, element) in buttonLists.enumerated() {
+            if index == currentIndex {
+                element.tintColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+            }
+            else {
+                element.tintColor = #colorLiteral(red: 0.6509803922, green: 0.6901960784, blue: 0.7294117647, alpha: 1)
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "PageViewController" {
+            guard let vc = segue.destination as? PageViewController else {return}
+            pageViewController = vc
+            
+            pageViewController.completeHandler = { (result) in
+                self.currentIndex = result
+            }
+        }
+    }
+    
+    @IBAction func homeTabButtonAction(_ sender: UIButton) {
+        pageViewController.setViewcontrollersFromIndex(index: 0)
+    }
+    
+    @IBAction func bestTabButtonAction(_ sender: UIButton) {
+        pageViewController.setViewcontrollersFromIndex(index: 1)
+    }
+    
+    @IBAction func newTabButtonAction(_ sender: UIButton) {
+        pageViewController.setViewcontrollersFromIndex(index: 2)
     }
     
 }
