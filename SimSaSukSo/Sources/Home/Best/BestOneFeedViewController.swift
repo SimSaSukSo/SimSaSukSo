0a4fe4483ac89f33c7bbb68b42856faaf5f6181c
so0nim <308go@naver.com>
2021-06-25 22:09:07 +0900

    Home탭 인기 레이아웃

---
+++ SimSaSukSo/Sources/Home/Best/BestOneFeedViewController.swift
@@ -0,0 +1,45 @@
+//
+//  BestOneFeedViewController.swift
+//  SimSaSukSo
+//
+//  Created by 소영 on 2021/06/25.
+//
+
+import UIKit
+
+class BestOneFeedViewController: UIViewController {
+    
+    @IBOutlet var bestOneFeedCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bestOneFeedCollectionView.delegate = self
+        bestOneFeedCollectionView.dataSource = self
+        
+    }
+    
+}
+
+//MARK: - CollectionView
+extension BestOneFeedViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeTabBestCollectionViewCell", for: indexPath)
+        
+        return cell
+    }
+}
+
+//MARK: - CollectionView FlowLayout
+extension BestOneFeedViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 375, height: 429)
+    }
+    
+}
---
+++ SimSaSukSo/Sources/Home/Best/HomeTabBestCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeTabBestCollectionViewCell.swift
+//  SimSaSukSo
+//
+//  Created by 소영 on 2021/06/25.
+//
+
+import UIKit
+
+class HomeTabBestCollectionViewCell: UICollectionViewCell {
+    
+}
