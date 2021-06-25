f38a4cf4f385728d55125334b1b842dd851da092
so0nim <308go@naver.com>
2021-06-25 17:09:11 +0900

    modify 병합

--- SimSaSukSo/Sources/Home/BestTabViewController.swift
+++ SimSaSukSo/Sources/Home/BestTabViewController.swift
@@ -12,7 +12,7 @@ class BestTabViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
