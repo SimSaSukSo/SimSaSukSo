3edbd335896c88d4ca9f005fd605bf109194bd76
이현서 <>
2021-07-03 17:11:52 +0900

    [Add] UploadGeneralPageOne 레이아웃

---
+++ SimSaSukSo/Sources/Upload/UploadGeneralPageOne.swift
@@ -0,0 +1,40 @@
+//
+//  UploadGeneralPageOne.swift
+//  SimSaSukSo
+//
+//  Created by 이현서 on 2021/07/03.
+//
+
+import UIKit
+class UploadGeneralPageOne : UIViewController{
+    
+    
+    @IBOutlet weak var HotelNameTextField: UITextField!
+    
+    @IBOutlet weak var UplodGeneralPictureCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UplodGeneralPictureCollectionView.dataSource = self
+        UplodGeneralPictureCollectionView.delegate = self
+    }
+    
+}
+
+extension UploadGeneralPageOne : UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let uploadGeneralcell = collectionView.dequeueReusableCell(withReuseIdentifier: "UploadGeneralPictureCollectionViewCell", for: indexPath) as! UploadGeneralPictureCollectionViewCell
+        
+        return uploadGeneralcell
+    }
+    
+    
+    
+    
+    
+    
+}
