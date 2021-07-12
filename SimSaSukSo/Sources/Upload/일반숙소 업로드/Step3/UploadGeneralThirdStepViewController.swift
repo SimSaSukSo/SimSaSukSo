5862fc2d2872a83fd2d3e94b76a7616312144874
이현서 <>
2021-07-12 17:16:00 +0900

    [Update] upload - step3 - 버튼 동작

--- SimSaSukSo/Sources/Upload/일반숙소 업로드/Step3/UploadGeneralThirdStepViewController.swift
+++ SimSaSukSo/Sources/Upload/일반숙소 업로드/Step3/UploadGeneralThirdStepViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 class UploadGeneralThirdStepViewController : UIViewController{
     
     @IBOutlet weak var thirdPictureCollectionView: UICollectionView!
-    @IBOutlet weak var usedToolCameraButton: AdaptableSizeButton!
     
+    @IBOutlet weak var usedToolCameraButton: AdaptableSizeButton!
     @IBOutlet weak var usedToolAppButton: AdaptableSizeButton!
     @IBOutlet weak var usedToolFilterButton: AdaptableSizeButton!
     @IBOutlet weak var usedToolSelfButton: AdaptableSizeButton!
@@ -45,6 +45,7 @@ class UploadGeneralThirdStepViewController : UIViewController{
         usedToolCameraButton.layer.borderColor = #colorLiteral(red: 0.9058823529, green: 0.9137254902, blue: 0.9215686275, alpha: 1)
         usedToolCameraButton.layer.cornerRadius = 4
         
+        
         usedToolAppButton.layer.borderWidth = 1
         usedToolAppButton.layer.borderColor = #colorLiteral(red: 0.9058823529, green: 0.9137254902, blue: 0.9215686275, alpha: 1)
         usedToolAppButton.layer.cornerRadius = 4
@@ -67,6 +68,77 @@ class UploadGeneralThirdStepViewController : UIViewController{
         
         
         
+    }
+    
+    @IBAction func selectButton(sender: AnyObject) {
+        guard let button = sender as? UIButton else {
+            return
+        }
+
+        switch button.tag {
+        case 0:
+            usedToolCameraButton.isSelected = !usedToolCameraButton.isSelected
+            if usedToolCameraButton.isSelected {
+                usedToolCameraButton.setTitleColor(.simsasuksoGreen, for: .selected)
+                usedToolCameraButton.layer.borderColor = #colorLiteral(red: 0, green: 0.8431372549, blue: 0.6705882353, alpha: 1)
+                usedToolCameraButton.backgroundColor = #colorLiteral(red: 0, green: 0.8431372549, blue: 0.6705882353, alpha: 0.1)
+            } else {
+                usedToolCameraButton.setTitleColor(#colorLiteral(red: 0.4352941176, green: 0.4705882353, blue: 0.5215686275, alpha: 1), for: .normal)
+                usedToolCameraButton.layer.borderColor = #colorLiteral(red: 0.9058823529, green: 0.9137254902, blue: 0.9215686275, alpha: 1)
+                usedToolCameraButton.backgroundColor = .clear
+                
+            }
+            
+            
+            break
+        case 1:
+            usedToolAppButton.isSelected = !usedToolAppButton.isSelected
+            if usedToolAppButton.isSelected {
+                usedToolAppButton.setTitleColor(.simsasuksoGreen, for: .selected)
+                usedToolAppButton.layer.borderColor = #colorLiteral(red: 0, green: 0.8431372549, blue: 0.6705882353, alpha: 1)
+                usedToolAppButton.backgroundColor = #colorLiteral(red: 0, green: 0.8431372549, blue: 0.6705882353, alpha: 0.1)
+            } else {
+                usedToolAppButton.setTitleColor(#colorLiteral(red: 0.4352941176, green: 0.4705882353, blue: 0.5215686275, alpha: 1), for: .normal)
+                usedToolAppButton.layer.borderColor = #colorLiteral(red: 0.9058823529, green: 0.9137254902, blue: 0.9215686275, alpha: 1)
+                usedToolAppButton.backgroundColor = .clear
+                
+            }
+            
+            break
+            
+        case 2:
+            usedToolFilterButton.isSelected = !usedToolFilterButton.isSelected
+            if usedToolFilterButton.isSelected {
+                usedToolFilterButton.setTitleColor(.simsasuksoGreen, for: .selected)
+                usedToolFilterButton.layer.borderColor = #colorLiteral(red: 0, green: 0.8431372549, blue: 0.6705882353, alpha: 1)
+                usedToolFilterButton.backgroundColor = #colorLiteral(red: 0, green: 0.8431372549, blue: 0.6705882353, alpha: 0.1)
+            } else {
+                usedToolFilterButton.setTitleColor(#colorLiteral(red: 0.4352941176, green: 0.4705882353, blue: 0.5215686275, alpha: 1), for: .normal)
+                usedToolFilterButton.layer.borderColor = #colorLiteral(red: 0.9058823529, green: 0.9137254902, blue: 0.9215686275, alpha: 1)
+                usedToolFilterButton.backgroundColor = .clear
+                
+            }
+            
+            break
+           
+        case 3:
+            usedToolSelfButton.isSelected = !usedToolSelfButton.isSelected
+            if usedToolSelfButton.isSelected {
+                usedToolSelfButton.setTitleColor(.simsasuksoGreen, for: .selected)
+                usedToolSelfButton.layer.borderColor = #colorLiteral(red: 0, green: 0.8431372549, blue: 0.6705882353, alpha: 1)
+                usedToolSelfButton.backgroundColor = #colorLiteral(red: 0, green: 0.8431372549, blue: 0.6705882353, alpha: 0.1)
+            } else {
+                usedToolSelfButton.setTitleColor(#colorLiteral(red: 0.4352941176, green: 0.4705882353, blue: 0.5215686275, alpha: 1), for: .normal)
+                usedToolSelfButton.layer.borderColor = #colorLiteral(red: 0.9058823529, green: 0.9137254902, blue: 0.9215686275, alpha: 1)
+                usedToolSelfButton.backgroundColor = .clear
+                
+            }
+            
+            break
+        default:
+            print("버튼 선택 안함")
+            return
+        }
     }
     
     
