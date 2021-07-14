2a03a12e98ba006ecb70aa2531fb2c0722f076f5
이현서 <>
2021-07-14 18:32:48 +0900

    [Update] upload - 지역선택 - 지역별 테이블뷰 전환

--- SimSaSukSo/Sources/Upload/에어비엔비 업로드/Step2/SelectRegionViewController.swift
+++ SimSaSukSo/Sources/Upload/에어비엔비 업로드/Step2/SelectRegionViewController.swift
@@ -12,7 +12,11 @@ class SelectRegionViewController : UIViewController{
     
     @IBOutlet weak var viewHeight: NSLayoutConstraint!
     var regionList : Array<String> = []
-    var seoulList = ["TJDSD","DFSDF","DFSDF","SDSDFSD"]
+    var tagList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+    var seoulList = ["강남 / 역삼 / 삼성","서초 / 교대 / 방배","사당","신사 / 압구정 / 청담","잠실 / 송파 / 잠실새내 / 천호 / 강동","서울역 / 이태원 / 용산","명동 / 중구","을지로 / 충무로","종로 / 동대문 / 대학로","인사동","서대문 / 마포 / 신촌 / 홍대","영등포","여의도","신림 / 서울대","구로 / 금천","김포공항","강서 / 화곡","건대 / 성수","왕십리","군자 / 상봉 / 중랑구","장안 / 청량리","강북 / 수유 / 도봉 / 미아 / 성북 / 노원"]
+    var busanList = ["부산","부산"]
+    var jejuList = ["제주제주","제주"]
+    var gangwonList = ["강원","강원강원"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +30,108 @@ class SelectRegionViewController : UIViewController{
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
     }
+    
+    @IBAction func regionButtonClicked(_ sender: UIButton) {
+        switch sender.tag{
+        case 1 :
+            regionList = seoulList
+            setFontnBackground(sender: sender)
+            break
+        case 2 :
+            regionList = busanList
+            setFontnBackground(sender: sender)
+            break
+         
+        case 3 :
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 4 :
+            regionList = gangwonList
+            setFontnBackground(sender: sender)
+            break
+        case 5 :
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 6 :
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 7 :
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 8 :
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 9 :
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 10:
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 11:
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 12:
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 13:
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 14:
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 15:
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 16:
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        case 17:
+            regionList = jejuList
+            setFontnBackground(sender: sender)
+            break
+        default:
+            print("")
+        }
+        
+    }
+    
+    func setFontnBackground(sender : UIButton){
+        //눌러진 버튼 세팅
+        let sender = sender
+        self.regionTableView.reloadData()
+        sender.titleLabel?.font = UIFont(name: "NotoSansCJKKR-Bold", size: 14)
+        sender.setTitleColor(#colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1), for: .normal)
+        sender.backgroundColor = .white
+        
+        //눌러진 버튼 이외 버튼 세팅
+        tagList.remove(at: sender.tag - 1)
+        for i in tagList{
+            let tmpButton = self.view.viewWithTag(i) as? UIButton
+            tmpButton?.titleLabel?.font = UIFont(name: "Noto Sans CJK KR", size: 14)
+            tmpButton!.setTitleColor(#colorLiteral(red: 0.6509803922, green: 0.6901960784, blue: 0.7294117647, alpha: 1), for: .normal)
+            tmpButton?.backgroundColor = #colorLiteral(red: 0.9725490196, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+            
+        }
+        tagList.append(sender.tag)
+        tagList.sort()
+        print(tagList)
+        
+        
+    }
+    
     
     @objc func cellClicked(_ sender: UIButton) {
         
