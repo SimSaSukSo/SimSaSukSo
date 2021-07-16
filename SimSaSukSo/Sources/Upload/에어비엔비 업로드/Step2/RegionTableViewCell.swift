099f59867a57ba9bd2128a733baa877e93d1c0c8
이현서 <>
2021-07-16 20:50:19 +0900

    no message

--- SimSaSukSo/Sources/Upload/에어비엔비 업로드/Step2/RegionTableViewCell.swift
+++ SimSaSukSo/Sources/Upload/에어비엔비 업로드/Step2/RegionTableViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class RegionTableViewCell: UITableViewCell {
     
-
+    
     @IBOutlet weak var regionButton: UIButton!
     
     override func awakeFromNib() {
@@ -17,23 +17,43 @@ class RegionTableViewCell: UITableViewCell {
         // Initialization code
     }
 
+//    var isTouched:Bool?{
+//
+//        didSet{
+//            if isTouched == true{
+//
+//                regionButton.setTitleColor(.simsasuksoGreen, for: .normal)
+//
+//
+//            }else{
+//                regionButton.setTitleColor(#colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1), for: .normal)
+//
+//
+//                
+//            }
+//
+//
+//
+//        }
+//    }
+//    @IBAction func cellClicked(_ sender: UIButton) {
+//       
+//        sender.isSelected = !sender.isSelected
+//        if sender.isSelected {
+//            SelectRegionViewController.clickList[sender.tag] = 1
+//            print(clickList)
+//           // sender.setTitleColor(.simsasuksoGreen, for: .selected)
+//            
+//            
+//        } else {
+//            //sender.setTitleColor(#colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1), for: .normal)
+//            
+//            
+//        }
+//        }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-    @IBAction func cellClicked(_ sender: UIButton) {
-        
-        sender.isSelected = !sender.isSelected
-        if sender.isSelected {
-            sender.setTitleColor(.simsasuksoGreen, for: .selected)
-            
-            
-        } else {
-            sender.setTitleColor(#colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1), for: .normal)
-            
-            
-        }
-        }
 
 }
