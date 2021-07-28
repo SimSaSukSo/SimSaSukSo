6e2ae2a63c37c79d0e4aba188a7cd3528dae214e
이현서 <>
2021-07-28 21:56:06 +0900

    [Modify] BestFeeds, BestOneFeed Paging

--- SimSaSukSo/Sources/Home/Best/BestDataManager.swift
+++ SimSaSukSo/Sources/Home/Best/BestDataManager.swift
@@ -31,12 +31,17 @@ class BestDataManager {
         let parameters : Parameters = [
             "page": page
         ]
-        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: KeyCenter.header)
+        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.queryString, headers: KeyCenter.header)
             .validate()
             .responseDecodable(of: BestResponse.self) { response in
                 switch response.result {
                 case .success(let response):
-                    delegate.bestOneFeed(result: response.result!)
+                    if page == 1{
+                        delegate.bestOneFeed(result: response.result!)
+                    }else{
+                        delegate.addBestOneFeed(result: response.result!)
+                    }
+                    
                 case .failure(let error):
                     print(error.localizedDescription)
                     delegate.failedToRequest(message: "서버와의 연결이 원활하지 않습니다")
@@ -45,14 +50,22 @@ class BestDataManager {
     }
     
     // 인기 - Feeds
-    func bestFeeds(delegate: BestFeedsViewController, url: String) {
-        //let url = "\(Constant.BASE_URL)api/feeds/hot?page=1"
-        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: KeyCenter.header)
+    func bestFeeds(page: Int, delegate: BestFeedsViewController) {
+        let url = "\(Constant.BASE_URL)api/feeds/hot"
+        let parameters : Parameters = [
+            "page": page
+        ]
+        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.queryString, headers: KeyCenter.header)
             .validate()
             .responseDecodable(of: BestResponse.self) { response in
                 switch response.result {
                 case .success(let response):
-                    delegate.bestFeeds(result: response.result!)
+                    if page == 1{
+                        delegate.bestFeeds(result: response.result!)
+                    }else{
+                        delegate.addBestFeeds(result: response.result!)
+                    }
+                    
                 case .failure(let error):
                     print(error.localizedDescription)
                     delegate.failedToRequest(message: "서버와의 연결이 원활하지 않습니다")
